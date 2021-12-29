0c7ebbf896b1b706d045f0265eacad9f5fac883e
Leon Lukashevsky <leon.lukashevsky@gmail.com>
2021-12-29 13:22:27 -0500

    the screen IS the button

--- src/App.svelte
+++ src/App.svelte
@@ -6,6 +6,14 @@
 	export let title;
 	let carousel;
 
+	function goToPrevPage() {
+		carousel.goToPrev();
+	}
+
+	function goToNextPage() {
+		carousel.goToNext();
+	}
+
 	let firstVerse = hereIAm[0];
 	let firstCouplet = firstVerse.couplets[0];
 	let firstCoupletWords = [...firstCouplet.a.split(' ')];
@@ -18,11 +26,15 @@
 </svelte:head>
 
 <main>
-	<Carousel bind:this={carousel} swipe={false} duration={5000}>
+	<Carousel bind:this={carousel} arrows={false} swiping={false} duration={5000}>
 		{#each firstCoupletWords as word}
-			<div class='magnifier'>{ word }</div>
+		<div class='magnifier'>{ word }</div>
 		{/each}
 	</Carousel>
+	<div class="navigation">
+		<div class="step" on:click={goToPrevPage}></div>
+		<div class="step" on:click={goToNextPage}></div>
+	</div>
 </main>
 
 <style>
@@ -39,6 +51,22 @@
 		width: 100%;
 		font-size: 60px;
 		text-align: center;
+	}
+
+	.navigation {
+		position: absolute;
+		top: 0;
+		bottom: 0;
+		left: 0;
+		right: 0;
+		display: flex;
+		flex-direction: row;
+		/* border: black 1px solid; */
+	}
+	
+	.step {
+		height: 100%;
+		flex-grow: 1;
 		/* border: dotted red 1px; */
 	}
 </style>
